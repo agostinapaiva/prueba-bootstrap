5e45e98d6119a225efecb08031f8bcd07ecf0c44
agostinapaiva <agospg7@gmail.com>
2024-08-28 18:36:37 -0300

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,38 @@
+let categoria = '101'
+
+function showProductsList(array) {
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+        htmlContentToAppend += `
+        <div class="product-item" >
+            <div class="product-image">
+                <img src=" `+ product.image +` " alt="product image" class="img-thumbnail">
+            </div>
+            <div class="product-details">
+                <p class="product-name">`+ product.name + `<p>
+                <p> ` + product.description + `</p>
+                <p> Precio $: ` + product.cost + `</p>
+                <p> Cantidad vendida: `+ product.soldCount + `</p>
+            </div>
+        </div>
+        `;
+    }
+
+    document.getElementById("products-container").innerHTML = htmlContentToAppend;
+}
+
+// Cargar los datos cuando el DOM esté listo
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL + '/' + categoria + '.json').then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data.products;
+            showProductsList(productsArray);
+        }
+    });
+});
+
+
+
